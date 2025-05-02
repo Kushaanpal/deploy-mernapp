1dd007d0245f1f86113565a7478bf012cbefbe13
Kushaanpal <160519585+Kushaanpal@users.noreply.github.com>
2025-05-02 20:29:33 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
     }
     const fetchProducts=async()=>{
         try{
-            const url="http://localhost:8080/products";
+            const url="deploy-mernapp-api-eight.vercel.app/products";
             const headers={
                 headers:{
                     'Authorization':localStorage.getItem('token')
